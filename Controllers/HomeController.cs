56a01be18ecd6bc2739129ddde9c58538b531acf
zk <15261300969@163.com>
2026-06-25 14:16:58 +0800

    添加项目文件。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+        }
+    }
+}
